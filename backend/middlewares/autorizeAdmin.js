01c4c96e3ae647e304a68c7ecfeefb8ea87562cf
Pritesh Mishra <priteshmishra2125@gmail.com>
2025-01-28 17:46:30 +0530

    resolving deployomg problem

--- backend/middlewares/autorizeAdmin.js
+++ backend/middlewares/autorizeAdmin.js
@@ -1,9 +1,8 @@
 export const authorizeAdmin = (req, res, next) => {
-    
     if (req.user.email !== "admin@ex.com") {
       return res.status(403).json({
         message: "Only admin allowed.",
       });
     }
     next();  
-  };
+};
